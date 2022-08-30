fe3c0a5a452a2e43984624ca431daa2da37b969f
mahimur <hellomahimur@gmail.com>
2022-08-30 12:33:21 +0600

    search input field enter key handler added

--- js/app.js
+++ js/app.js
@@ -40,6 +40,7 @@ const displayPhones = (phones, dataLimit) => {
                             <h5 class="card-title">${phone.phone_name}</h5>
                             <p class="card-text">This is a longer card with supporting text below as a natural
                                 lead-in to additional content. This content is a little bit longer.</p>
+                                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
                         </div>
                     </div>
         `;
@@ -62,6 +63,15 @@ document.getElementById('btn-search').addEventListener('click', function () {
     processSearch(10);
 });
 
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function (event) {
+    // console.log(event.key);
+    if (event.key === 'Enter') {
+        // code for enter
+        processSearch(10);
+    }
+});
+
 const toggoleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
     if (isLoading) {
@@ -75,5 +85,12 @@ const toggoleSpinner = isLoading => {
 document.getElementById('btn-show-all').addEventListener('click', function () {
     processSearch();
 });
+
+const loadPhoneDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data)
+}
 
 // loadPhones();
